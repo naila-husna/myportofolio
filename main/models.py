9d789e7a9781b1e1861a7c70685f826490d31097
naila-husna <naila.husna@ui.ac.id>
2026-09-09 22:18:19 +0700

    finish tutor 2

--- main/models.py
+++ main/models.py
@@ -12,7 +12,7 @@ class Experience(models.Model):
         ('freelance', 'Freelance'),
     ]
     
-    
+      
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
