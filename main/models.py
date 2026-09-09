15500e2d2271ebe0f36e3ab318b817e2ec747895
naila-husna <naila.husna@ui.ac.id>
2026-09-09 22:14:18 +0700

    update pws 2.0

--- main/models.py
+++ main/models.py
@@ -12,6 +12,8 @@ class Experience(models.Model):
         ('freelance', 'Freelance'),
     ]
     
+    
+    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField()
